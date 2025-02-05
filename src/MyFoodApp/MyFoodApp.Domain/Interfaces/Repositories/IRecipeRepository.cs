1074ec2549760c2b3571a22ef3957176875464be
dmckay <dmckay@managewaresolutions.com>
2025-02-04 20:38:54 -0700

    DM - Display Fixed Cyclic Query

--- src/MyFoodApp/MyFoodApp.Domain/Interfaces/Repositories/IRecipeRepository.cs
+++ src/MyFoodApp/MyFoodApp.Domain/Interfaces/Repositories/IRecipeRepository.cs
@@ -5,7 +5,8 @@ namespace MyFoodApp.Domain.Interfaces.Repositories
     public interface IRecipeRepository
     {
         IQueryable<Recipe> GetAllRecipesAsync();
-        Task<Recipe?> GetRecipeByIdAsync(int recipeId, bool tracking = false);
+        Task<Recipe?> GetRecipeByIdAsync(int recipeId);
+        Task<Recipe?> GetRecipeByIdAsync(int recipeId, bool tracking);
         Task<Recipe> AddRecipeAsync(Recipe recipe);
         Task AddRecipeStepRangeAsync(List<RecipeStep> items);
         Task AddIngredientRangeAsync(List<Ingredient> items);
--- src/MyFoodApp/MyFoodApp.Infrastructure/Repositories/RecipeRepository.cs
+++ src/MyFoodApp/MyFoodApp.Infrastructure/Repositories/RecipeRepository.cs
@@ -23,12 +23,13 @@ namespace MyFoodApp.Infrastructure.Repositories
                         .ThenInclude(f => f!.FoodCategory)
                 .Include(r => r.MealSuggestions)
                     .ThenInclude(ms => ms.MealSuggestion)
+                .AsNoTracking()
                 .AsQueryable();
 
             return recipes;
         }
 
-        public async Task<Recipe?> GetRecipeByIdAsync(int id, bool tracking = false)
+        public async Task<Recipe?> GetRecipeByIdAsync(int id)
         {
             var query = _context.Recipes
                 .Include(r => r.Steps)
@@ -36,7 +37,21 @@ namespace MyFoodApp.Infrastructure.Repositories
                     .ThenInclude(i => i.FoodItem)
                         .ThenInclude(f => f!.FoodCategory)
                 .Include(r => r.MealSuggestions)
-                    .ThenInclude(ms => ms.MealSuggestion);
+                    .ThenInclude(ms => ms.MealSuggestion)
+                        .ThenInclude(ms => ms.RecipeSuggestions)
+                            .ThenInclude(ms => ms.Recipe);
+
+            var recipe = await query.FirstOrDefaultAsync(r => r.RecipeId == id);
+
+            return recipe;
+        }
+
+        public async Task<Recipe?> GetRecipeByIdAsync(int id, bool tracking)
+        {
+            var query = _context.Recipes
+                .Include(r => r.Steps)
+                .Include(r => r.Ingredients)
+                .Include(r => r.MealSuggestions);
 
             return tracking
                 ? await query.FirstOrDefaultAsync(r => r.RecipeId == id)
