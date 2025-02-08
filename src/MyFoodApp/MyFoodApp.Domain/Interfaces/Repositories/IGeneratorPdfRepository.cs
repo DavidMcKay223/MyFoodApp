d73470e5de3c4c65bf37fca5149b9a9af7a4ef32
dmckay <dmckay@managewaresolutions.com>
2025-02-07 18:42:06 -0700

    DM - Download Works for PDF Generation for Selecting

--- src/MyFoodApp/MyFoodApp.Domain/Interfaces/Repositories/IGeneratorPdfRepository.cs
+++ src/MyFoodApp/MyFoodApp.Domain/Interfaces/Repositories/IGeneratorPdfRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyFoodApp.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace MyFoodApp.Domain.Interfaces.Repositories
 {
     public interface IGeneratorPdfRepository
     {
+        Task<List<Recipe>> GetAllRecipesByIdListAsync(List<int> idList);
     }
 }
--- src/MyFoodApp/MyFoodApp.Infrastructure/Repositories/GeneratorPdfRepository.cs
+++ src/MyFoodApp/MyFoodApp.Infrastructure/Repositories/GeneratorPdfRepository.cs
@@ -1,4 +1,7 @@
-﻿using MyFoodApp.Domain.Interfaces.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using MyFoodApp.Domain.Entities;
+using MyFoodApp.Domain.Interfaces.Repositories;
+using MyFoodApp.Infrastructure.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +12,29 @@ namespace MyFoodApp.Infrastructure.Repositories
 {
     public class GeneratorPdfRepository : IGeneratorPdfRepository
     {
+        private readonly AppDbContext _context;
+
+        public GeneratorPdfRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Recipe>> GetAllRecipesByIdListAsync(List<int> idList)
+        {
+            return await _context.Recipes
+                .Include(r => r.Steps)
+                .Include(r => r.Ingredients)
+                    .ThenInclude(i => i.FoodItem)
+                        .ThenInclude(f => f.FoodCategory)
+                .Include(r => r.Ingredients)
+                    .ThenInclude(i => i.FoodItem)
+                        .ThenInclude(f => f.PriceHistories)
+                .Include(r => r.Ingredients)
+                    .ThenInclude(i => i.FoodItem)
+                        .ThenInclude(f => f.StoreSections)
+                            .ThenInclude(fi => fi.StoreSection)
+                .Where(x => idList.Contains(x.RecipeId))
+                .ToListAsync();
+        }
     }
 }
